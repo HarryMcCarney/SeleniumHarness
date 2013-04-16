12674e2b33d9324e800869eb6407b945e953be01
Maarten Balliauw <maarten@balliauw.be>
2013-04-16 14:41:34 +0200

    Various changes
    - Moved project into SeleniumHarness subfolder
    - Added solution file
    - Enabled NuGet package restore

---
+++ SeleniumHarness/Tests/Login.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Login
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [SetUp]
+        public void SetupTest()
+        {
+                driver = new FirefoxDriver();
+                baseURL = "http://www.larryslist.de/";
+                verificationErrors = new StringBuilder();
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [Test]
+        public void TheLoginTest()
+        {
+            driver.Navigate().GoToUrl("http://www.larryslist.de/admin/logout");
+            driver.FindElement(By.Name("formdata.email")).Clear();
+            driver.FindElement(By.Name("formdata.email")).SendKeys("mapa@friendfund.com");
+            driver.FindElement(By.Name("formdata.pwd")).Clear();
+            driver.FindElement(By.Name("formdata.pwd")).SendKeys("mapa123");
+            driver.FindElement(By.CssSelector("button.btn.btn-primary")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alert.Text;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
